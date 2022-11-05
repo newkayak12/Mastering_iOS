13232220e5e71ff981b7e045fabe5f13a160e964
newkayak12 <newkayak12@gmail.com>
2022-11-05 22:07:49 +0900

    masteringiOS

---
+++ 1-Hello/HelloXcode/HelloXcode/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by Sang Hyeon kim on 2022/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    /**
+      에러 대처
+     step1. 코드 확인
+     step2. UI 확인
+     step3. Xcode 재실행
+     step4. mac 재부팅
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let a = 10
+        print(a)
+        
+    }
+
+
+}
+
