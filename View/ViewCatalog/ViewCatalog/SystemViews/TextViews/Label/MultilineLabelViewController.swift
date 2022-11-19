6aa85d58bf796555e1558a6fb95ce04434542980
newkayak12 <newkayak12@gmail.com>
2022-11-19 23:57:03 +0900

    masteringiOS

--- View/ViewCatalog/ViewCatalog/SystemViews/TextViews/Label/MultilineLabelViewController.swift
+++ View/ViewCatalog/ViewCatalog/SystemViews/TextViews/Label/MultilineLabelViewController.swift
@@ -1,26 +1,3 @@
-//
-//  Mastering iOS
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import UIKit
 
 class MultilineLabelViewController: UIViewController {
@@ -35,15 +12,20 @@ class MultilineLabelViewController: UIViewController {
     
     
     @IBAction func updateLines(_ sender: UIStepper) {
-        
+        valueLabel.numberOfLines = Int(sender.value)
+        lineNumberLabel.text = "\(valueLabel.numberOfLines)"
     }
     
     @IBAction func toggleAutoshrink(_ sender: UISwitch) {
-        
+        valueLabel.minimumScaleFactor = sender.isOn ? 0.5 : 0.0
+        valueLabel.adjustsFontSizeToFitWidth = sender.isOn
+        //autoShrink는 위의 두 가지 속성을 한 번에 바꿔야한다.
     }
     
     func changeLineBreakMode(with rawValue: Int) {
-        
+        if let mode = NSLineBreakMode(rawValue: rawValue) {
+            valueLabel.lineBreakMode = mode
+        }
     }
     
     
@@ -51,7 +33,14 @@ class MultilineLabelViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        lineNumberLabel.text = "\(valueLabel.numberOfLines)"
+        lineNumberStepper.value = Double(valueLabel.numberOfLines)
         
+        autoshrinkSwitch.isOn = valueLabel.minimumScaleFactor != 0 &&
+        valueLabel.adjustsFontSizeToFitWidth
+        
+        lineBreakModePicker.selectRow(valueLabel.lineBreakMode.rawValue,
+                                        inComponent: 0, animated:false)
         
     }
 }
--- View/ViewCatalog/ViewCatalog/SystemViews/TextViews/TextField/TextFieldViewController.swift
+++ View/ViewCatalog/ViewCatalog/SystemViews/TextViews/TextField/TextFieldViewController.swift
@@ -1,33 +1,23 @@
-//
-//  Mastering iOS
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import UIKit
 
 class TextFieldViewController: UIViewController {
     
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    @IBAction func report(_ sender: Any) {
+        guard let text = inputField.text, text.count > 0  else {
+            return
+        }
+        valueLabel.text = text
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        inputField.placeholder = "input Value"
+        valueLabel.text = inputField.text ?? ""
         
     }
 }
