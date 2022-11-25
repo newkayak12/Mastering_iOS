3efe2ca04f5a5feba37fd56352caab0dd9100ae4
newkayak12 <newkayak12@gmail.com>
2022-11-25 18:35:45 +0900

    masteringiOS

--- Date/Date.playground/Pages/TimeInterval.xcplaygroundpage/Contents.swift
+++ Date/Date.playground/Pages/TimeInterval.xcplaygroundpage/Contents.swift
@@ -1,25 +1,4 @@
-//
-//  Mastering iOS
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
+
 //: [Previous](@previous)
 
 import Foundation
@@ -28,6 +7,15 @@ import Foundation
  # TimeInterval
 */
 
-
-
+let oneSec = TimeInterval(1)
+print(oneSec)
+let oneMilliSec = TimeInterval(0.001)
+let oneMin = TimeInterval(60)
+let oneHour = TimeInterval(60 * 60)
+let oneDay = TimeInterval(oneHour * 24)
+Date(timeIntervalSinceNow: oneDay)
+Date() //Now
+//Date(timeIntervalSinceReferenceDate: ) //2001-01-01 기준
+//Date(timeIntervalSince1970: ) // epoch
+//Date(timeInterval: <#T##TimeInterval#>, since: <#T##Date#>) //custom
 //: [Next](@next)
---
+++ Text/PasswordAutoFill/PasswordAutoFill/README.md
@@ -0,0 +1,12 @@
+#  Associated Domain
++ button -> Domain 입력 
+domain/.well-known/apple-app-site-associtation
+domain/apple-app-site-association => 
+```json
+    webcredentials : { 
+        apps: {
+                "~~":"~~"
+        }
+    }
+```
+이러면 도메인과 연결됨 (비밀번호 autofill은 해당 도메인과 관련되어 비밀번호가 자동으로 fill)
