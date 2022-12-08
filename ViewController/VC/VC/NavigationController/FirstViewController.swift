d90cffc1735aa401463c75359089efc3d128fa38
newkayak12 <newkayak12@gmail.com>
2022-12-08 23:57:35 +0900

    segue

--- ViewController/VC/VC/NavigationController/FirstViewController.swift
+++ ViewController/VC/VC/NavigationController/FirstViewController.swift
@@ -3,6 +3,11 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    @IBAction func unwindToFirst(_ unwindSegue: UIStoryboardSegue) {
+//        let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+    }
+    
     @IBAction func pushSecond(_ sender: Any) {
         guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") else {return}
         navigationController?.pushViewController(secondVC, animated: true)
--- ViewController/VC/VC/NavigationController/SecondViewController.swift
+++ ViewController/VC/VC/NavigationController/SecondViewController.swift
@@ -9,7 +9,21 @@ class SecondViewController: UIViewController {
     
     @IBAction func pushThird(_ sender: Any) {
         guard let thirdVC = storyboard?.instantiateViewController(withIdentifier: "ThirdViewController") else {return}
-        navigationController?.pushViewController(thirdVC, animated: true)
+//        navigationController?.pushViewController(thirdVC, animated: false)
+        
+        guard let fourthVC = storyboard?.instantiateViewController(withIdentifier: "FourthViewController") else {return}
+//        navigationController?.pushViewController(fourthVC, animated: false)
+        
+        //2 -> 5 -> 4
+        guard let fifthVC = storyboard?.instantiateViewController(withIdentifier: "FifthViewController") else {return}
+        navigationController?.pushViewController(fifthVC, animated: true)
+        
+        if var navigationStack = navigationController?.viewControllers, let index = navigationStack.firstIndex(of: fifthVC){
+            navigationStack.insert(fourthVC, at: index)
+            navigationStack.insert(thirdVC, at: index)
+            navigationController?.viewControllers = navigationStack
+            //네비게이션 스택 직접 조작
+        }
     }
     
     
