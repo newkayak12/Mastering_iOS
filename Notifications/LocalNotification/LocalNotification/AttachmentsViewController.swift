5d619a4610cbf3a5177b70ec8088507f5e6bc540
newkayak12 <newkayak12@gmail.com>
2022-12-14 22:28:25 +0900

    localNotification

--- Notifications/LocalNotification/LocalNotification/AttachmentsViewController.swift
+++ Notifications/LocalNotification/LocalNotification/AttachmentsViewController.swift
@@ -1,25 +1,3 @@
-//
-//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import UIKit
 import UserNotifications
 
@@ -30,10 +8,19 @@ class AttachmentsViewController: UIViewController {
       content.title = "Hello"
       content.body = "Image Attachment"
       content.sound = UNNotificationSound.default()
+       guard let url = Bundle.main.url(forResource: "hello", withExtension: "png") else {return}
+       var options = [UNNotificationAttachmentOptionsThumbnailHiddenKey:true]
+       guard let imageAttachment = try?
+                UNNotificationAttachment(identifier: "hello-image", url: url, options: options) else {return}
+       
+       guard let thumbUrl = Bundle.main.url(forResource: "logo", withExtension: "png") else {return}
+       options = [UNNotificationAttachmentOptionsThumbnailHiddenKey:false]
+       guard let thumbnailAttachment = try?
+                UNNotificationAttachment(identifier: "thumbnail-image", url: thumbUrl, options: options) else{return}
+       content.attachments = [imageAttachment, thumbnailAttachment]
       
       
-      
-      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
       let request = UNNotificationRequest(identifier: "Image Attachment", content: content, trigger: trigger)
       UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
    }
@@ -43,10 +30,15 @@ class AttachmentsViewController: UIViewController {
       content.title = "Hello"
       content.body = "Audio Attachment"
       content.sound = UNNotificationSound.default()
+       
+       guard let url = Bundle.main.url(forResource: "bell", withExtension: "aif") else {return}
+       guard let audioAttachment = try? UNNotificationAttachment(identifier: "audio", url: url) else {return}
+       
+       content.attachments = [audioAttachment]
       
       
       
-      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
       let request = UNNotificationRequest(identifier: "Audio Attachment", content: content, trigger: trigger)
       UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
    }
@@ -57,10 +49,12 @@ class AttachmentsViewController: UIViewController {
       content.body = "Video Attachment"
       content.sound = UNNotificationSound.default()
 
+       guard let url = Bundle.main.url(forResource: "video", withExtension: "mp4") else {return}
+       guard let videoAttachment = try? UNNotificationAttachment(identifier: "video", url: url) else {return}
+       content.attachments = [videoAttachment]
       
       
-      
-      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
       let request = UNNotificationRequest(identifier: "Video Attachment", content: content, trigger: trigger)      
       UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
    }
