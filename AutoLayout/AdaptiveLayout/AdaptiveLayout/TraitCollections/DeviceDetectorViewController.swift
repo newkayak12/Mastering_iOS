4e214d243b4c0831d373fd99f8e0bc56191681ca
newkayak12 <newkayak12@gmail.com>
2022-12-24 19:18:56 +0900

    autoLayout

--- AutoLayout/AdaptiveLayout/AdaptiveLayout/TraitCollections/DeviceDetectorViewController.swift
+++ AutoLayout/AdaptiveLayout/AdaptiveLayout/TraitCollections/DeviceDetectorViewController.swift
@@ -1,25 +1,3 @@
-//
-//  Mastering iOS
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
 
 import UIKit
 
@@ -28,13 +6,74 @@ class DeviceDetectorViewController: UIViewController {
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var deviceImageView: UIImageView!
     @IBOutlet weak var deviceNameLabel: UILabel!
-    
+
     func update() {
-        
+        var uiImage: UIImage? = UIImage(systemName: "questionmark.circle") ?? nil
+        var text = "???"
+        switch traitCollection.userInterfaceIdiom {
+        case .unspecified:
+            uiImage = UIImage(systemName: "questionmark.circle")
+            text = "???"
+        case .phone:
+            uiImage = UIImage(named: "iPhone")
+            text = "iPhone"
+        case .pad:
+            uiImage = UIImage(named: "iPad")
+            text = "iPad"
+        case .tv:
+            uiImage = UIImage(named: "appleTv")
+            text = "Apple Tv"
+        case .carPlay:
+            uiImage = UIImage(systemName: "car")
+            text = "Car"
+        case .mac:
+            uiImage = UIImage(named: "iMac")
+            text = "Mac"
+        @unknown default:
+            uiImage = UIImage(systemName: "questionmark.circle")
+            text = "???"
+        }
+        deviceImageView.image = uiImage
+        deviceNameLabel.text = text
     }
 
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        print(#function)
+        //보통
+        if newCollection.verticalSizeClass == .compact {
+            stackView.axis = .horizontal
+        } else {
+            stackView.axis = .vertical
+        }
+        
+        if traitCollection.userInterfaceStyle == .dark {
+            view.backgroundColor = .systemIndigo
+        } else {
+            view.backgroundColor = .yellow
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        //iPad 역시
+        super.viewWillTransition(to: size, with: coordinator)
+        if size.width > size.height {
+            stackView.axis = .horizontal
+        } else {
+            stackView.axis = .vertical
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        print(#function)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        update()
     }
 }
