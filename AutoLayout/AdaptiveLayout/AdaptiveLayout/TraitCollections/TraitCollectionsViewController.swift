6bcd7ae99f0c2e842bdb47721d5b7db975b7f0d2
newkayak12 <newkayak12@gmail.com>
2022-12-24 18:50:09 +0900

    autoLayout

--- AutoLayout/AdaptiveLayout/AdaptiveLayout/TraitCollections/TraitCollectionsViewController.swift
+++ AutoLayout/AdaptiveLayout/AdaptiveLayout/TraitCollections/TraitCollectionsViewController.swift
@@ -1,26 +1,3 @@
-//
-//  Mastering iOS
-//  Copyright (c) KxCoding <help@kxcoding.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
 import UIKit
 
 struct Item {
@@ -35,14 +12,35 @@ class TraitCollectionsViewController: UIViewController {
     
     func update() {
         list.removeAll()
-        
+        /**
+         UITraitEnvironment
+            trailCollection
+            traitCollectionDidChange
+         
+         UIContentContainer
+            viewWillTransition
+            willTransition
+         */
         list.append(contentsOf: [
-            
+//            UITraitCollection.current
+            Item(title: "Horizontal Size Class", value: traitCollection.horizontalSizeClass.description),
+            Item(title: "Vertical Size Class", value: traitCollection.verticalSizeClass.description),
+            Item(title: "Display Scale", value: "\(traitCollection.displayScale)"),
+            Item(title: "Display Gamut", value: "\(traitCollection.displayGamut)"),
+            Item(title: "Interface Style", value: "\(traitCollection.userInterfaceStyle.description)"),
+            Item(title: "Interface Idiom", value: "\(traitCollection.userInterfaceIdiom.description)"),
         ])
      
         listTableView.reloadData()
     }
-
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        update()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
